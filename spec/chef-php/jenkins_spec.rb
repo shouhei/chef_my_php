9bb7c6bc2a8d7e92f59c8f533f17e991bfc99e9e
山口将平 <shouhei.yamaguchi.be@gmail.com>
2015-01-04 19:52:36 +0900

    add jenkins_spec

---
+++ spec/chef-php/jenkins_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe package('java-1.7.0-openjdk-1.7.0.71-2.5.3.2.el6_6.x86_64') do
+  it { should be_installed }
+end
+
+describe package('jenkins-1.595-1.1.noarch') do
+  it { should be_installed }
+end
